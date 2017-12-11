cd4392e4e7d93c4e52a69d3d8b6a90edc7f67030
erdem koral <koral.erdem@gmail.com>
2017-12-11 14:54:35 -0800

    tiny fix

--- src/home/UserInfo.js
+++ src/home/UserInfo.js
@@ -8,7 +8,7 @@ class UserInfo extends Component {
                 <h1 className="title">Home</h1>
                 <h2 className="subtitle">
                     Dear <strong>$ValuableUser</strong>, <br />
-                    You have donated <span>$thisMuch</span> breast milk,
+                    You have donated <span>$thisMuch🍼🍼🍼🍼</span> breast milk,
                     and saved <span>$thisMany</span> lives.
                      Thank you for being a part of <span>Northwest Mothers Milk Bank</span>.
                 </h2>
