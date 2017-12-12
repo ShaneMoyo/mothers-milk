fd385de4f27d8a204029d2a3ead52b5507234e89
Shane Moyo <smoyo@pdx.edu>
2017-12-12 14:19:14 -0800

    add superagent to package.json

--- src/services/request.js
+++ src/services/request.js
@@ -1,5 +1,6 @@
 import store from '../store/store';
 import { API_URL } from './constants';
+import superagent from 'superagent'
 
 let headers = new Headers();
 let token = '';
@@ -17,8 +18,16 @@ store.subscribe(() => {
 
 export const getStoredToken = () => storage.token;
 
-// const wrap = cmd => {
-// };
+const wrap = cmd => cmd 
+  .set('Authorization', token)
+  .then(
+    res => res.body,
+    ({ response }) => {
+      const { body, text } = response;
+      const error = body ? body.message || body.error || body : text;
+      throw error;
+    }
+  )
 
 
 export const request = {
