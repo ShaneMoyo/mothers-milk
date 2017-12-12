0f43c3afc3ae4bbd8f6420b095a747e6dc4e5688
mugworks <greenwood.pdx@gmail.com>
2017-12-11 18:35:19 -0800

    add input boxes for supplies

--- src/supplyRequest/SupplyRequest.js
+++ src/supplyRequest/SupplyRequest.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
 
+
 class SupplyRequest extends Component {
+  state = {
+    target: 'off'
+  }
+
+  // handleChecked = (event) => {
+  //   console.log('event target', event.target.value);
+  //   const checked = event.target.value;
+  //   this.setState({ target: checked });
+  // }
+
+  handleSubmit = (supplies) => {
+    
+  }
+
   render() {
-    return(
+    const { target } = this.state;
+    return (
+
       <div className="tile is-4 is-vertical is-parent">
         <div className="tile is-child box hero is-primary">
-          <div className="sub-title"><strong>Request Shipping Materials</strong></div>
-          <form>
+          <div className="sub-title"><strong>Request Supplies</strong></div>
+          <form onSubmit={this.handleSubmit}>
             <div className="field">
               <div className="control">
-                <div className="input-title">Shipping Materials</div> <input type="checkbox"/>
-                <div className="input-title">Storage Materials</div> <input type="checkbox"/>
+                <div className="input-title">
+                  <label>Milk Collection Bags: </label>
+                  <select name="bags">
+                    <option value="2">2</option>
+                    <option value="4">4</option>
+                    <option value="6">6</option>
+                    <option value="8">8</option>
+                    <option value="10">10</option>
+                  </select>
+                </div>
+                {/* <input type="checkbox" onChange={this.handleChecked} /> */}
+                {/* {(target === 'on') && <InputBox/>} */}
+                <div className="input-title">
+                  <label>Shipping Boxes: </label>
+                  <select name="boxes">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                  </select>
+                </div> 
+                {/* <input type="checkbox" /> */}
               </div>
-              <button className="button" >Submit</button>
-            </div>  
+              <button className="button" onSubmit={this.handleSubmit}>Submit</button>
+            </div>
           </form>
         </div>
         <div className="tile is-child box hero is-success">
@@ -22,6 +59,19 @@ class SupplyRequest extends Component {
       </div>
     );
   }
+}
+
+class InputBox extends Component {
+  render() {
+    const { target, onChange } = this.props;
+    return(
+      <div>
+        <br/>
+        <label>Quantity: <input type="text"/></label>
+      </div>
+    );
+  }
+
 }
 
 export default SupplyRequest;
