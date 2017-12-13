8598d7363e1957c2611189b79e70048fb7fec381
Shane Moyo <smoyo@pdx.edu>
2017-12-13 06:21:18 -0800

    refactor DropSites.js

--- src/dropSites/DropSites.js
+++ src/dropSites/DropSites.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
 import { loadDropSites } from '../dropSites/actions';
 import { connect } from 'react-redux';
+import { dropSites } from './reducer';
 
 
 class DropSites extends Component {
 
-  componentDidMount() {
-    console.log('did mount drop sites');
-    //this.props.loadDropSites();
-  }
-
   render() {
 
     const { dropSites } = this.props;
-
+    const listDropSites = dropSites.map((dropSite) => (
+      <li key={dropSite._id}>
+        {dropSite.name}<br/>
+        {dropSite.address}<br/>
+        {dropSite.hours}
+      </li>
+    ));
     return (
       <div className="tile is-parent">
         <div className="tile is-child box">
           <h2 className="subtitle">Locations</h2>
           <ul>
-            {dropSites.map((dropSite) => (
-              <li key={dropSite._id}>
-                {dropSite.name}<br/>
-                {dropSite.address}<br/>
-                {dropSite.hours}
-              </li>
-            ))}
+            {listDropSites}
           </ul>
         </div>
       </div>
@@ -33,13 +29,8 @@ class DropSites extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    dropSites: state.dropSites
-  };
-}
 
 export default connect(
-  mapStateToProps,
+  ({ dropSites }) => ({ dropSites }),
   { loadDropSites }
 )(DropSites);
