3cdd69c07f0b1cef37fb9cd25d3a87b08d5b26e8
Shane Moyo <smoyo@pdx.edu>
2017-12-13 07:40:46 -0800

    add loadDonations action creator

--- src/donations/actions.js
+++ src/donations/actions.js
@@ -1,3 +1,12 @@
 import * as actions from './constants';
 import donationApi from '../services/donation-api';
 import { getStoredToken } from '../services/request';
+
+export function loadDonations() {
+  return dispatch => {
+    dispatch({
+      type: actions.LOAD_DONATOINS,
+      payload: donationApi.get()
+    });
+  };
+}
---
+++ src/donations/constants.js
@@ -0,0 +1,1 @@
+export const LOAD_DONATION = 'LOAD_DONATION';
