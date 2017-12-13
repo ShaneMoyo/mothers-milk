35fe6f75230a173c10bba2f7e40c5d1b776b4883
Shane Moyo <smoyo@pdx.edu>
2017-12-13 07:33:26 -0800

    create handle donate method on Donation.js

--- src/donations/Donations.js
+++ src/donations/Donations.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { donate } from './actions';
 
 class MakeDonation extends Component {
+
+  handleDonate = event => {
+    event.preventDefault();
+    const { dropSite, quantity } = event.target.elements;
+    this.props.signin(
+      { 
+        quantity: quantity.value,
+        dropSite: dropSite.value 
+      });
+  }
 
   render(){
     return (
       <div>
-        <form onSubmit={event => this.handleSignIn(event)}>
+        <form onSubmit={event => this.handleDonate(event)}>
           <label>quantity: <input name="quantity"/></label>
           <label>drop site: <input name="dropSite"/></label>
           <input type="submit" ></input>
