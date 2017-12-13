15c0b3579dbcbf0734a2867e47d49a5dcd283a30
Shane Moyo <smoyo@pdx.edu>
2017-12-13 07:55:42 -0800

    add donations to root reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import auth from '../auth/reducers';
 import { dropSites } from '../dropSites/reducer';
+import donations from '../donations/reducer';
 import { error, loading } from '../services/reducer';
 
 export default combineReducers({
   auth,
+  donations,
   dropSites,
   error,
   loading
