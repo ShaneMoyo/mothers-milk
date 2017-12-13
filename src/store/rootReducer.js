b9f61de0200ba53c08f95973fa74533810a86492
Shane Moyo <smoyo@pdx.edu>
2017-12-13 06:39:12 -0800

    push to remote

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 import auth from '../auth/reducers';
-
 import { dropSites } from '../dropSites/reducer';
 import { error, loading } from '../services/reducer';
-
 
 export default combineReducers({
   auth,
