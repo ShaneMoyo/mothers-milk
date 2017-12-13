0763356dbd1a18a5d35bd99b9e94feb289867388
Shane Moyo <smoyo@pdx.edu>
2017-12-13 06:26:52 -0800

    refactor Home.js

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,33 +1,25 @@
 import React, { Component } from 'react';
 import HomeInfo from './HomeInfo';
-
 import DropSites from '../dropSites/DropSites';
 import SupplyRequest from '../supplyRequest/SupplyRequest';
 import Header from './Header';
 import Footer from './Footer';
-
-import { checkForToken } from '../auth/actions';
 import { loadDropSites } from '../dropSites/actions';
-
 import { connect } from 'react-redux';
-
 import '../style/mystyle.css';
 
 
 class Home extends Component {
 
-  componentDidMount() {
-    // this.props.checkForToken();
-    // this.props.loadDropSites();
-  }
-
   render() {
-    const { users } = this.props;
-    users && this.props.loadDropSites();
+
+    const { user } = this.props;
+    user && this.props.loadDropSites();
+
     return (
       <div>
         <Header/>
-        <h1>hello { users ? users.name : 'no user' }</h1>
+        <h1>hello { user ? user.name : 'no user' }</h1>
         <HomeInfo/>
         <div className="need-space"></div>
         <div className="container is-fluid">
@@ -44,13 +36,9 @@ class Home extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    users: state.auth.user
-  };
-}
+
 
 export default connect(
-  mapStateToProps,
-  {  checkForToken, loadDropSites }
+  ({ auth }) => ({ user: auth.user }),
+  { loadDropSites }
 )(Home);
