b0212693971a1fda6d82950ae63bbef18e0b95c4
Shane Moyo <smoyo@pdx.edu>
2017-12-13 06:12:54 -0800

    removed siginin.js

--- src/auth/constants.js
+++ src/auth/constants.js
@@ -2,5 +2,4 @@ export const CHECKED_TOKEN = 'CHECKED_TOKEN';
 export const GOT_TOKEN = 'GOT_TOKEN';
 export const AUTH_FAILED = 'AUTH_FAILED'; 
 export const FETCHED_USER = 'FETCHED_USER';
-export const FETCH_USER = 'FETCH_USER';
 export const LOGOUT = 'LOGOUT';
