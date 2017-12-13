77b41965997946678371ada4e6728fc13b56addd
Shane Moyo <smoyo@pdx.edu>
2017-12-13 07:30:44 -0800

    add form for make donation

--- src/donations/Donations.js
+++ src/donations/Donations.js
@@ -1,2 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+class MakeDonation extends Component {
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={event => this.handleSignIn(event)}>
+          <label>quantity: <input name="quantity"/></label>
+          <label>drop site: <input name="dropSite"/></label>
+          <input type="submit" ></input>
+        </form>
+      </div>
+    );
+  }
+}
