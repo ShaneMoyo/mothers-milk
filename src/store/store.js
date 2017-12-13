504a13dc7d2f4dc513fac2ece70d99c7bf914a87
Shane Moyo <smoyo@pdx.edu>
2017-12-13 04:48:29 -0800

    dropsites loading After token fetch

--- src/store/store.js
+++ src/store/store.js
@@ -5,12 +5,17 @@ import rootReducer from './rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const logger = store => next => action => {
+  console.log('before action', action);
+  return next(action);
+};
 
 const store = createStore(
   rootReducer,
   {}, 
   composeEnhancers(
     applyMiddleware(
+      logger,
       thunk,
       promiseMiddleware
     )
