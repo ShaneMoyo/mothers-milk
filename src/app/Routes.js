72c249d2c850301e36518c60efc96cadab6e600f
Shane Moyo <smoyo@pdx.edu>
2017-12-14 04:39:56 -0800

    now exporting routes from Routes.js

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -7,7 +7,7 @@ import {
 
 import Home from '../home/Home';
 import Auth from '../auth/Auth';
-import Admin
+import Admin from '../admin/Admin';
 
 import PrivateRoute from './PrivateRoute';
 
@@ -15,7 +15,7 @@ export default () => (
   <Switch> 
     <Route exact path="/" render={() => <Auth/>}/>
     <PrivateRoute exact path="/home" component={Home}/>;
-    <PrivateRoute path="/admin" render={({ match }) => <Admin id={match.params.id}/>}/>;
+    <PrivateRoute path="/admin" component={Admin}/>;
     <Redirect to="/"/>
   </Switch>  
 );
