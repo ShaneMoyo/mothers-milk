c7df8ef481e25aab8453da2675c75116d3ced484
Shane Moyo <smoyo@pdx.edu>
2017-12-16 17:46:46 -0800

    made donations table static

--- src/donations/AllDonations.js
+++ src/donations/AllDonations.js
@@ -35,15 +35,20 @@ class AllDonations extends PureComponent {
 
     const tabledonations = donations.length ? donations.map(item => {
       let rowdonations = Object.values(item).filter(item => item !== null);
-      const id = rowdonations.shift();
+      console.log('item', item);
+      console.log('rowdonations', rowdonations);
       rowdonations = rowdonations.filter(data => data !== 0);
       const row = rowdonations.map((value, index) => <li style={{ display:'inline', margin:'5px' }}>{this.fieldCheck(value)}</li>);
       return (
         <ul>
-          {row}
-          <li style={{ display:'inline' }}><input type="button" value="X" onClick={() => this.handleDelete(id)}/></li>
-          <li style={{ display:'inline' }}><input type="button" value="✎" onClick={() => this.setState({ editing: id, show: !this.state.show })}/></li>
-          {((editing === id) && (this.state.show)) && 
+          {/* {row} */}
+          <li style={{ display:'inline', margin:'5px' }}>{item.donor.name}</li>
+          <li style={{ display:'inline', margin:'5px' }}>{item.dropSite.name}</li>
+          <li style={{ display:'inline', margin:'5px' }}>{item.quantity}</li>
+          <li style={{ display:'inline', margin:'5px' }}>{item.status}</li>
+          <li style={{ display:'inline' }}><input type="button" value="X" onClick={() => this.handleDelete(item._id)}/></li>
+          <li style={{ display:'inline' }}><input type="button" value="✎" onClick={() => this.setState({ editing: item._id, show: !this.state.show })}/></li>
+          {((editing === item._id) && (this.state.show)) && 
           <li>
             <form onSubmit={event => this.handleUpdate(event, item)}>
               <select name="status">
@@ -72,3 +77,48 @@ export default connect(
   ({ auth, donations }) => ({ user: auth.user, donations }),
   { loadDonations, updateDonation, deleteDonation }
 )(AllDonations);
+
+// render() {
+//   const { donations } = this.props;
+//   const { editing } = this.state;
+
+//   const tabledonations = donations.length ? donations.map(item => {
+//     let rowdonations = Object.values(item).filter(item => item !== null);
+//     console.log('item', item);
+//     console.log('rowdonations', rowdonations);
+//     rowdonations = rowdonations.filter(data => data !== 0);
+//     const row = rowdonations.map((value, index) => <li style={{ display:'inline', margin:'5px' }}>{this.fieldCheck(value)}</li>);
+//     return (
+//       <ul>
+//         {row}
+//         <li style={{ display:'inline' }}><input type="button" value="X" onClick={() => this.handleDelete(item._id)}/></li>
+//         <li style={{ display:'inline' }}><input type="button" value="✎" onClick={() => this.setState({ editing: item._id, show: !this.state.show })}/></li>
+//         {((editing === item._id) && (this.state.show)) && 
+//         <li>
+//           <form onSubmit={event => this.handleUpdate(event, item)}>
+//             <select name="status">
+//               <option key="0" value="Pending">Pending</option>
+//               <option key="1" value="Received">Received</option>
+//               <option key="2" value="Missing">Missing</option>
+//             </select>
+//             <input type="text" name="quantityReceived" placeholder="quantityReceived"/>
+//             <input type="submit"/>
+//           </form>
+//         </li>}
+//       </ul>
+//     );
+//   }): null;
+  
+//   return(
+//     <div className="column is-6 is-offset-3">
+//       <h3 className="title is-4">Donations</h3>
+//       {tabledonations}
+//     </div>
+//   );
+// }
+// }
+
+// export default connect(
+// ({ auth, donations }) => ({ user: auth.user, donations }),
+// { loadDonations, updateDonation, deleteDonation }
+// )(AllDonations);
